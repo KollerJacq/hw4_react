c46d7c901d7ffb22d7ec06c362b4f1a5877bfff5
KollerJacq <147981831+KollerJacq@users.noreply.github.com>
2023-12-27 19:27:21 +0100

    Switched the footer to an own js-file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import "./styles.css";
 import Input from "./input";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
       <Input />
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/KollerJacq" target="_blank">
-          Jacqueline Koller
-        </a>{" "}
-        and is opend-sourced on{" "}
-        <a href="https://github.com/KollerJacq/hw4_react" target="_blank">
-          GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a href="https://creative-sprite-45cc80.netlify.app/" target="_blank">
-          Netlify
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      This project was coded by{" "}
+      <a href="https://github.com/KollerJacq" target="_blank">
+        Jacqueline Koller
+      </a>{" "}
+      and is opend-sourced on{" "}
+      <a href="https://github.com/KollerJacq/hw4_react" target="_blank">
+        GitHub
+      </a>{" "}
+      and hosted on{" "}
+      <a href="https://creative-sprite-45cc80.netlify.app/" target="_blank">
+        Netlify
+      </a>
+    </footer>
+  );
+}
