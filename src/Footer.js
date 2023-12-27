e73a911f76d5ceb130ff3a9b533d6d7df4d35c60
KollerJacq <147981831+KollerJacq@users.noreply.github.com>
2023-12-27 19:31:43 +0100

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -10,10 +10,6 @@ export default function Footer() {
       and is opend-sourced on{" "}
       <a href="https://github.com/KollerJacq/hw4_react" target="_blank">
         GitHub
-      </a>{" "}
-      and hosted on{" "}
-      <a href="https://creative-sprite-45cc80.netlify.app/" target="_blank">
-        Netlify
       </a>
     </footer>
   );
