6d723eb71797bb676f3f13770299910adaca55b4
KollerJacq <147981831+KollerJacq@users.noreply.github.com>
2023-12-27 19:15:40 +0100

    Added footer and some css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Input />
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/KollerJacq" target="_blank">
+          Jacqueline Koller
+        </a>{" "}
+        and is opend-sourced on{" "}
+        <a href="https://github.com/KollerJacq/hw4_react" target="_blank">
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://creative-sprite-45cc80.netlify.app/" target="_blank">
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
